dff00d40a300f323f19e5839fa88a77a8cb489d6
Berni <pbernal@mendoza.edu.ar>
2025-09-04 21:47:01 -0300

    Obtener datos y escritura en html

---
+++ c11_bucles/login.js
@@ -0,0 +1,57 @@
+
+// 1.capturamos el botón
+let botonLogin = document.getElementById("login");
+
+// veamos el botonLogin
+console.log(botonLogin);
+
+
+// 2. Agrego un evento al elemento, callback: una función que recibe de argumento otra función
+botonLogin.addEventListener("click", login)
+
+// opción b, con una función anónima
+//botonLogin.addEventListener("click", function() {})
+
+// función que ejecuta el login
+function login(e) {
+    //alert("Login correcto")
+
+    e.preventDefault(); // previene la ejecución del evento
+
+    console.log(e);
+    
+
+    //1. Capturamos el input del email 
+    let email = document.getElementById("email").value;
+    console.log(email);
+
+    //2. capturamos el input del password
+    let password = document.getElementById("password").value;
+    console.log(password);
+    
+
+    //3. procesamos los datos
+    if(email == "admin@gmail.com" && password == "12345678") {
+        alert("Bienvenido Admin Pepe");
+        location.href = "dom.html";
+    }else{
+        alert("Contraseña o Email incorrectos");
+        location.href="error.html";
+    }
+    
+}
+
+
+
+
+
+
+/* (()=>{
+    console.log("Soy anónimo");
+})() */
+
+/* const flecha = () => {}
+
+let pepe = function () {
+    alert("algo");
+} */
