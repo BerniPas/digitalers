eb7ab2587cc17fefa87626cc646d5a8061ae83ce
Berni <pbernal@mendoza.edu.ar>
2025-09-16 21:58:18 -0300

    DOM con Navegación

--- c12_dom/index.js
+++ c12_dom/index.js
@@ -17,6 +17,8 @@ ul.className = "navbar-nav";
 // 6. Creamos los li dentro del ul
 let li = document.createElement("li");
 
+let li2 = document.createElement("li");
+
 // 7. le agregamos las clases de bootstrap al li
 li.className = "nav-item";
 
@@ -30,9 +32,17 @@ a.textContent = "Login";
 a.ariaCurrent = "page";
 a.style = "margin-left: 40px; text-transform: uppercase;"; // le agregamos un margen al a
 
+let b = document.createElement("a");
+b.className = "navbar-brand";
+b.href = "index.html";
+b.textContent = "Inicio";
+b.ariaCurrent = "page";
+b.style = "margin-left: 40px; text-transform: uppercase;"; // le agregamos un margen al b
 
 // 10. Agregamos a su contenedor padre todos los elementos creados
 li.appendChild(a);
+li2.appendChild(b);
 ul.appendChild(li);
+ul.appendChild(li2);
 nav.appendChild(ul);
 root.appendChild(nav);
